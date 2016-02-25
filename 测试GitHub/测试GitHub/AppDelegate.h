8361bf296dd121384b4238af328c3526f5be9649
administrator <shinasahi@jxmacbook-air.local>
2016-02-25 23:40:53 +0800

    初始化项目

---
+++ 测试GitHub/测试GitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试GitHub
+//
+//  Created by TwistFate on 16/2/25.
+//  Copyright © 2016年 TwistFate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试GitHub/测试GitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试GitHub
+//
+//  Created by TwistFate on 16/2/25.
+//  Copyright © 2016年 TwistFate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
